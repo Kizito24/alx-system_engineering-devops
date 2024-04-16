b1a9b07d5864ae1697d39294de739fae45b7de68
Chinoyelum Kizito Chiazor <kizitochiazor@gmail.com>
2024-04-16 09:01:32 +0000

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# A puppet file to install flask:
-exec { 'pip3 install flask==2.1.0':
-  path    => '/usr/bin:/usr/sbin:/bin',
-  returns => '0',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# A puppet file to kill a process calles killmenow
-exec { 'pkill -9 killmenow':
-  path  => 'usr/local/bin:/usr/bin:/usr/sbin:/bin',
-}
