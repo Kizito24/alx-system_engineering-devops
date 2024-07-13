4b6bd622f351cd679e99bef8407fb2146bc9b109
Kizito <kizitochiazor@gmail.com>
2024-07-13 16:32:45 +0100

    Task fixes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# A puppet file to install flask:
-exec { 'pip3 install flask==2.1.0':
-  path    => '/usr/bin:/usr/sbin:/bin',
-  returns => '0',
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
